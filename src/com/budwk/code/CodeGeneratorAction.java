4221a67d00ae1486170d2ad57f9f9a17dc1be4aa
Wizzer <wizzer@qq.com>
2021-03-22 11:08:57 +0800

    init

---
+++ src/com/budwk/code/CodeGeneratorAction.java
@@ -0,0 +1,12 @@
+package com.budwk.code;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class CodeGeneratorAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
